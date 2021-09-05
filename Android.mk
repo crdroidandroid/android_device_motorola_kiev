369843f00e1c4771f1d1c74ca4f9adc9b126f125
Andrzej Perczak <kartapolska@gmail.com>
2021-09-05 17:35:52 +0800

    kiev: Symlink libvndfwk_detect_jni.qti for CneApp
    Needed by CneApp.
    
    dlopen failed: library "/system_ext/lib64/libvndfwk_detect_jni.qti.so" needed or dlopened by "/apex/com.android.art/lib64/libnativeloader.so" is not accessible for the namespace "vendor-classloader-namespace"
    
    Change-Id: I400f03c3cb60acdc4263c24b1096c5961142f03f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- Android.mk
+++ Android.mk
@@ -140,4 +140,14 @@ $(EXPAT_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(EXPAT_SYMLINKS)
 
+CNE_LIBS := libvndfwk_detect_jni.qti.so
+CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/,$(notdir $(CNE_LIBS)))
+$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "CNE lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
+
 endif
