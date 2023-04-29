00e1ab745b73e030c10264811ed443efff220819
Pranav Vashi <neobuddy89@gmail.com>
2023-04-29 09:34:07 +0530

    kiev: Fix extract from LOS directly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,7 +72,7 @@ function blob_fixup() {
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
         ;;
     system_ext/etc/permissions/moto-telephony.xml)
-        sed -i "s|system|system/system_ext|" "${2}"
+        sed -i "s|system/framework|system/system_ext/framework|" "${2}"
         ;;
     # Patch configureRpcThreadpool
     vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so | vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so)
